14ea03c0bc34b6b9cbe21adb67fc863218e942c6
GoPotatos <awabelmahe@gmail.com>
2020-12-13 16:27:26 +0200

    Adding files

---
+++ content.js
@@ -0,0 +1,5 @@
+    const nav=document.createElement("div");
+    nav.style.backgroundColor="aqua";
+    nav.style.borderBottom="1px solid black";
+    nav.innerHTML='<a href="#">Home</a>  <a href="#">Youtube</a> <a href="#">About</a>';
+    document.body.prepend(nav);
